1abf46d86fcb09d55fdfc21607c4532a7be86287
Teh Lemon <Teh-Lemon@users.noreply.github.com>
2015-05-13 17:34:38 +0100

    Added particle effect to AmmoRefill powerup

--- Assets/_Scripts/PowerUps/AmmoRefillPowerUp.cs
+++ Assets/_Scripts/PowerUps/AmmoRefillPowerUp.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class AmmoRefillPowerUp : PowerUp
 {
+    [SerializeField]
+    ParticleSystem flashEffect;
+
     public override void Activate()
     {
         PlayerController.Instance.FillUpAmmo();
+        flashEffect.Play();
         base.Activate();
     }
 }
